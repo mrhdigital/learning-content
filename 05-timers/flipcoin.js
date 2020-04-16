1a79e3f643cf98948dbf9449713ab328e9d92e1b
unknown <mrhdigital@yahoo.com>
2020-04-15 20:36:53 -0500

    update win counter

--- 05-timers/flipcoin.js
+++ 05-timers/flipcoin.js
@@ -17,7 +17,7 @@
 
       //  If randomNumber is equal to zero then:
             if (randomNumber === 0) {
-                $("$coin-image").html("<img src = 'http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg' />");
+                $("#coin-image").html("<img src = 'http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg' />");
             }
       //  Find the div with an id of coin-image.
       //  Replace its html with an img tag containing this image:
@@ -25,7 +25,7 @@
 
       // Else:
       else {
-        $("$coin-image").html("<img src = 'http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg' />");
+        $("#coin-image").html("<img src = 'http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg' />");
   
       }
 
@@ -40,16 +40,16 @@
       //  Increment wins by one.
       //  Find the div with an id of win-lose. Update it with an h2 of "Winner!"
          if (result === randomNumber) {
-             winn++;
+             wins++;
              $("#win-lose").html("<h2>Winner!</h2>");
-             $("#wins").html("<h3>" + winn + "</h3>");
+             $("#wins").html("<h3>" + wins + "</h3>");
          }
 
       // Else:
        else {
            losses++;
            $("#win-lose").html("<h2>Loser!</h2>");
-           $("#wins").html("<h3>" + losses + "</h3>");
+           $("#losses").html("<h3>" + losses + "</h3>");
 
 
        }
@@ -72,8 +72,12 @@
       //  STEP FOUR:
 
       //  Increment tailsCount by one.
+      tailsCount++;
       //  Find the span with an id of tails-chosen. Replace the html inside of the span with the tailsCount.
+      $("#tails-chosen").text(tailsCount);
       //  Find the div with an id of guess. Update it with the word "Tails" bolded.
+      $("#guess").html("<b>Tails</b>");
       //  Call the flipThatCoin function and pass value 1 into it.
+      flipThatCoin(1);
 
     });
