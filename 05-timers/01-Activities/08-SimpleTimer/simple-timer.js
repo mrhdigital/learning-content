0ae5673df46fad7ea22dfd6639542ed8a4aea0c5
unknown <mrhdigital@yahoo.com>
2020-04-18 15:11:06 -0500

    fixed the id #time-left

--- 05-timers/01-Activities/08-SimpleTimer/simple-timer.js
+++ 05-timers/01-Activities/08-SimpleTimer/simple-timer.js
@@ -1,6 +1,8 @@
 // Step 1:
       // Use the following link inside the Audio function below:
       // https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90
+      
+      
       var audio = new Audio("raven.mp3");
 
       //  Step 2: do the following between the --- comments
@@ -23,7 +25,7 @@
 
         // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
         // console log 10 seconds left
-        $("#timer-left").append("<h2> About 10 second Left!</h2>");
+        $("#time-left").append("<h2> About 10 second Left!</h2>");
         console.log("10 seconds left!");
         
     }
@@ -33,7 +35,7 @@
         
         // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
         // console log 5 seconds left
-        $("#timer-left").append("<h2> About 5 Seconds Left!</h2>");
+        $("#time-left").append("<h2> About 5 Seconds Left!</h2>");
         console.log("5 seconds left!");
         
     }
@@ -44,7 +46,7 @@
         // in the element with an id of time-left add an h2 saying Time's Up!
         // console log done
         
-        $("#timer-left").append("<h2> Time's UP!</h2>");
+        $("#time-left").append("<h2> Time's UP!</h2>");
         console.log("Done!");
         // The following line will play the audio file we linked to above.
         audio.play();
