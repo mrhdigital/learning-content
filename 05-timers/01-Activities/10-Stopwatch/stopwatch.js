f1db3039648e6056f3a269fe44fe2e4d9bde090b
unknown <mrhdigital@yahoo.com>
2020-04-21 15:53:44 -0500

    added with out the use of object

--- 05-timers/01-Activities/10-Stopwatch/stopwatch.js
+++ 05-timers/01-Activities/10-Stopwatch/stopwatch.js
@@ -96,4 +96,65 @@ window.onload = function() {
       return minutes + ":" + seconds;
     }
   };
+
+  // Solution if you choose not to put it in an object
+
+// var time = 0;
+// var lap = 1;
+// function reset() {
+
+//   time = 0;
+//   lap = 1;
+
+//   $("#display").text("00:00");
+//   $("#laps").text("");
+
+// }
+
+// function start() {
+//   intervalId = setInterval(count, 1000);
+// }
+
+// function stop() {
+
+//   console.log("stopping");
+//   clearInterval(intervalId);
+
+// }
+
+// function recordLap() {
+
+//   var converted = timeConverter(time);
+//   $("#laps").append("<p>Lap " + lap + " : " + converted + "</p>");
+//   lap++;
+
+// }
+
+// function count() {
+
+//   time++;
+//   var converted = timeConverter(time);
+//   $("#display").text(converted);
+
+// }
+
+// function timeConverter(t) {
+
+//   var minutes = Math.floor(t / 60);
+//   var seconds = t - (minutes * 60);
+
+//   if (seconds < 10) {
+//     seconds = "0" + seconds;
+//   }
+
+//   if (minutes === 0) {
+//     minutes = "00";
+//   }
+//   else if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
+
+//   return minutes + ":" + seconds;
+// }
+
   
