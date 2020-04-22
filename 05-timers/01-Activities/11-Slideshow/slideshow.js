448c476b48ae55ce2f19fc60317a8b041a8a46c1
unknown <mrhdigital@yahoo.com>
2020-04-22 15:53:57 -0500

    loading.gif loads between every other image

--- 05-timers/01-Activities/11-Slideshow/slideshow.js
+++ 05-timers/01-Activities/11-Slideshow/slideshow.js
@@ -28,7 +28,7 @@ function nextImage() {
 count++;
 
   // TODO: Show the loading gif in the "image-holder" div.
-$("#image-holder").html("<img src= 'images/loading.gif' width = '400px' />");
+$("#image-holder").html("<img src= 'images/loading.gif' width = '200px' />");
 
   // TODO: Use a setTimeout to run displayImage after 1 second.
 
