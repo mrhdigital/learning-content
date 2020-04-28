fb95a4f181a0a741d2c81e4a785bc9b94d03e2bb
unknown <mrhdigital@yahoo.com>
2020-04-28 15:43:49 -0500

    console log Buzz correctly

--- 05-timers/01-Activities/16-FizzBuzz/app.js
+++ 05-timers/01-Activities/16-FizzBuzz/app.js
@@ -6,6 +6,8 @@ var FizzBuzz = function() {
             console.log("FizzBuzz");
         } else if(i % 3 === 0) {
             console.log("Fizz");
+        } else if(i % 5 === 0) {
+            console.log("Buzz");
         }
         
         else{
