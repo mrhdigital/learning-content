91fc5ab43ca77c21f8b51664c0db439b3422ce2e
unknown <mrhdigital@yahoo.com>
2020-04-28 15:28:28 -0500

    console log FizzBuzz correctly

--- 05-timers/01-Activities/16-FizzBuzz/app.js
+++ 05-timers/01-Activities/16-FizzBuzz/app.js
@@ -1,6 +1,11 @@
 var FizzBuzz = function() {
  
     for(var i = 1; i <= 100; i++) {
+
+        if((i % 3 === 0) && (i % 5 === 0)){
+            console.log("FizzBuzz");
+        } else{
         console.log(i);
+        }
     }
 }
