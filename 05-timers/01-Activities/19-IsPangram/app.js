1a420a0a6e3bfbb32f5418b04eb1bd54bba44044
unknown <mrhdigital@yahoo.com>
2020-05-01 17:47:07 -0500

    split the string into array of letters

--- 05-timers/01-Activities/19-IsPangram/app.js
+++ 05-timers/01-Activities/19-IsPangram/app.js
@@ -4,5 +4,23 @@ var test1 = "The quick brown fox jumps over the lazy dog.";
 
 var isPangram = function(string) {
 
+    if(string.length < 26) {
+        console.log("False: It is NOT a pangram");
+    }
+ var letterHolder = [];
+
+ var letters = string.toLowerCase().replace(/[^a-z]+/g, "").split("");
+ console.log(letters);
+
+ for (var i = 0; i < string.length; i++) {
+     if (string[i].indexOf(letterHolder) === -1) {
+         console.log("push to letterHolder")
+     }
+ 
+
+
+    else {
     console.log("I will give answere here");
+    }
+}
 }
