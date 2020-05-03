550a66e9eb421365eca7c9923f8285986e0fa97c
unknown <mrhdigital@yahoo.com>
2020-05-03 18:10:19 -0500

    styles for task div

--- 05-timers/01-Activities/20-jQueryToDo/app.js
+++ 05-timers/01-Activities/20-jQueryToDo/app.js
@@ -1,8 +1,12 @@
-alert("linked");
+//alert("linked");
 /**
  * jQuery TODO
  **/
-
+// When the user clicks any button, run addTask.
+$("button").click(addTask);
+// Add a listener to the document.
+// It should keep an ear out for a click on elements with an id of "delete".
+// We can't use click, because the element is dynamically created
 /*
  *  Use the provided starter HTML to make a list app.
  *  This app will take text from an input box
