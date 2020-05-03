e19784ec5a484728a2b4e218ccb64fe9013e6800
unknown <mrhdigital@yahoo.com>
2020-05-03 18:18:11 -0500

    click event is created

--- 05-timers/01-Activities/20-jQueryToDo/app.js
+++ 05-timers/01-Activities/20-jQueryToDo/app.js
@@ -7,6 +7,16 @@ $("button").click(addTask);
 // Add a listener to the document.
 // It should keep an ear out for a click on elements with an id of "delete".
 // We can't use click, because the element is dynamically created
+
+
+// When a user presses any key on their keyboard,
+$("input").keypress(function(event) {
+
+    // listen to see that key was enter.
+    if (event.which == 13) {
+        addTask();
+    }
+});
 /*
  *  Use the provided starter HTML to make a list app.
  *  This app will take text from an input box
