ee527d6b0c8adc764b2cd24a0747966542c3b316
unknown <mrhdigital@yahoo.com>
2020-05-08 15:19:30 -0500

    backward loop

--- 05-timers/01-Activities/22-PalindromeDetector/app1.js
+++ 05-timers/01-Activities/22-PalindromeDetector/app1.js
@@ -2,11 +2,31 @@ var sentence1 = "Otto bought a racecar he cannot afford.";
 var sentence2 = "Otto lost the deed to his house.";
 var sentence3 = "Otto needs Xanax.";
 
-
+var letters = [];
 function stringToArrayOfWords(s) {
     var words = s.split(' ');
+
     console.log(words);
+    for(i = 0; i < words.length; i++){
+        words[i] = words[i].replace(/[^a-zA-Z]+/g, "");
+
+        letters =  words[i].split("");
+        document.getElementById("result").innerHTML = letters.join(" ");
+    
+        console.log(letters);
+    
         // Replace the contents of the "result" div with the merged showOnpage array.
         document.getElementById("result1").innerHTML = words.join(" ");
 
+        var reveredArray = [];
+        var reverseAndJoin = "";
+
+        for( var j = letters.length -1; j >= 0; J--) {
+            reverseAndJoin += letters[j];
+
+        }
+    }
+
 }
+
+
