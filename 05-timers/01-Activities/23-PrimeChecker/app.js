a5667b6d2fac44ee4c784b3eaaf80ddb43a1c49a
unknown <mrhdigital@yahoo.com>
2020-05-09 14:28:32 -0500

    for loop to check if number is divisible by other than one or itself

--- 05-timers/01-Activities/23-PrimeChecker/app.js
+++ 05-timers/01-Activities/23-PrimeChecker/app.js
@@ -1,8 +1,23 @@
 // Prime Checker
+//* Write a function that checks to see if a number is a prime number or not. Have it return `true` if it is, or `false` if it isn't.
+
+//* A Prime number is a number greater than one that can only be divided by one and itself.
 
 // Declare our primeCheck function
 function primeCheck(number) {
-
+if(number <= 0) {
+    //var isPrime = "";
+   var isPrime = (`${number} is not a prime number`);
+    console.log(isPrime);
+    document.getElementById("result").innerHTML = isPrime;
+}
+else 
+for( var i = 2; i < number; i++) {
+    if(number % i === 0) {
+        var isPrime = (`${number} is not a prime number`);
+        console.log(isPrime);
+        document.getElementById("result").innerHTML = isPrime;
+    }
+}
 }
 
-document.getElementById("result").innerHTML = "2";
