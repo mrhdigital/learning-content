b4382cbab62add196f2ffd9fd26bb59196856262
unknown <mrhdigital@yahoo.com>
2020-05-11 15:16:46 -0500

    fibonacci function

--- 05-timers/01-Activities/24-RecursiveFibonacci/recursiveFibSolution.js
+++ 05-timers/01-Activities/24-RecursiveFibonacci/recursiveFibSolution.js
@@ -7,13 +7,21 @@ var fibonacci = function(n) {
 
     }
     return answer;
-}
+};
+
+// Function which takes an integer as an argument (i) which is a place
+// in the Fibonacci Sequence
 var recursive = function(i) {
 
+    // Triggered when calculating the 1st and 0th places,
+    // simply returns 1 for the 1st position and
+    // 0 for the 0th
     if(i < 2) {
         return i;
     }
     else {
+
+        // Run resursive on the two places before the current one
         return recursive(i - 1) + recursive(i - 2);
     }
 };
