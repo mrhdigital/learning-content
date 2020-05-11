57add159ba90642c15febd0f6d94d39780cf3170
unknown <mrhdigital@yahoo.com>
2020-05-10 19:56:53 -0500

    simple recursion function

--- 05-timers/01-Activities/24-RecursiveFibonacci/simpleRecursion.js
+++ 05-timers/01-Activities/24-RecursiveFibonacci/simpleRecursion.js
@@ -1,14 +1,14 @@
 var simpleRecursion = function(n) {
 
     // If n is greater than zero
-     
+     if (n > 0) {
       // Log n to the console
-      
+      console.log(n);
   
       // Then call simpleRecursion again, and pass in n-1
       // This will run the function again with a number one less than n
       
-    
+     }
   
     
 }    
