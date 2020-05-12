4a92be9d4936fc106c981b95c7857fe36d5504de
unknown <mrhdigital@yahoo.com>
2020-05-12 15:53:23 -0500

    added index html

--- 05-timers/01-Activities/24-RecursiveFibonacci/fibonacci.js
+++ 05-timers/01-Activities/24-RecursiveFibonacci/fibonacci.js
@@ -1,23 +1,37 @@
 //console.log("link");
 // 1 1 2 3 5 8 
-var fibonacci = function(i) {
-    var result = [];
-    for (var j = 1; j < i +1; j++) {
-        result.push(recursive(j));
+// var fibonacci = function(i) {
+//     var result = [];
+//     for (var j = 1; j < i +1; j++) {
+//         result.push(recursive(j));
+//     }
+
+//     document.getElementById("result").innerHTML = result.join(" ");
+
+//     return result;
+
+// }
+
+// // Returns the fibonacci sequence integer
+// var recursive = function(n) {
+//     if(n < 2){
+//         return n;
+//     }
+//     else {
+//         return recursive(n - 1) + recursive(n - 2);
+//     }
+// };
+
+//console.log("linked")
+
+function fibonacci(number) {
+
+    var previous_first = 0, previous_second = 1, next = 1;
+
+    for(var i = 2; i <= number; i++) {
+        next = previous_first + previous_second;
+        previous_first = previous_second;
+        previous_second = next;
     }
-
-    document.getElementById("result").innerHTML = result.join(" ");
-
-    return result;
-
-}
-
-// Returns the fibonacci sequence integer
-var recursive = function(n) {
-    if(n < 2){
-        return n;
-    }
-    else {
-        return recursive(n - 1) + recursive(n - 2);
-    }
+    return next;
 };
