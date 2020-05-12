4cf0e0bbb56e0d3878a3bf606969d5a3da96f424
unknown <mrhdigital@yahoo.com>
2020-05-12 00:09:24 -0500

    fibonacci function displays in html

---
+++ 05-timers/01-Activities/24-RecursiveFibonacci/fibonacci.js
@@ -0,0 +1,20 @@
+//console.log("link");
+// 1 1 2 3 5 8 
+var fibonacci = function(i) {
+    var result = [];
+    for (var j = 1; j < i +1; j++) {
+        result.push(recursive(j));
+    }
+    return result;
+
+}
+
+// Returns the fibonacci sequence integer
+var recursive = function(n) {
+    if(n < 2){
+        return n;
+    }
+    else {
+        return recursive(n - 1) + recursive(n - 2);
+    }
+};
