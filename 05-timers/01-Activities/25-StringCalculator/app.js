79ed9711054347d0278be592df6a8488f2c8bad8
unknown <mrhdigital@yahoo.com>
2020-05-12 16:09:01 -0500

    split input into calculator words

--- 05-timers/01-Activities/25-StringCalculator/app.js
+++ 05-timers/01-Activities/25-StringCalculator/app.js
@@ -4,4 +4,14 @@ var test2 = "Subtract 3 from 10";
 var test3 = "Multiply 8 and 2";
 var test4 = "Divide 30 by 3";
 
+// Declare our string calculator function
+function stringCalculator(string) {
+
+    // Split input into calculator words
+    // Using toLoweCase to ensure it will match that
+    // we're checking in the swith statement.
+
+    var splitString = string.toLowerCase().split(" ");
+}
+
 
