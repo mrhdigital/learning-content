fbb6e1d257b780b7d091fba0054bcf647d09c137
unknown <mrhdigital@yahoo.com>
2020-05-12 16:36:05 -0500

    checked test1

--- 05-timers/01-Activities/25-StringCalculator/app.js
+++ 05-timers/01-Activities/25-StringCalculator/app.js
@@ -1,5 +1,5 @@
 // Test strings t run in our calculator
-var test1 = "Add 2 and 4";
+var test1 = "Add 2 and fout";
 var test2 = "Subtract 3 from 10";
 var test3 = "Multiply 8 and 2";
 var test4 = "Divide 30 by 3";
