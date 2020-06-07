37d3e2bc2f19a0cd918ae25c98fcec0016d8aaf6
unknown <mrhdigital@yahoo.com>
2020-06-06 20:11:59 -0500

    divide code into three sections

--- 06-ajax/01-Activities/16-NYTSearch/logic.js
+++ 06-ajax/01-Activities/16-NYTSearch/logic.js
@@ -1,1 +1,25 @@
 alert("HI");
+
+// SETUP VARIABLES
+// =========================================
+
+
+
+
+// FUNCTIONS
+// =========================================
+
+
+
+
+
+// MAIN PROCESSES
+// =========================================
+
+
+// 1. Retrieve user inputs and convert to variables
+// 2. Use those variable to run an AJAX call to the New York Times.
+// 3. Break down the NYT Object into useable fields
+// 4. Dynamically generate html content
+
+// 5. Dealing with "edge cases" -- bugs or situations that are not intuitive.
