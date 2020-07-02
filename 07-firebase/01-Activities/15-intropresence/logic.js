ac3ae735744a035c0d0943f0e220764e4fad4ce2
unknown <mrhdigital@yahoo.com>
2020-07-01 20:38:02 -0500

    initialize firebase

--- 07-firebase/01-Activities/15-intropresence/logic.js
+++ 07-firebase/01-Activities/15-intropresence/logic.js
@@ -2,6 +2,15 @@
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
 // (Ex. 3.0 != 3.7.0)
+var config = {
+  apiKey: "AIzaSyBE4rIfU_He-8pm0yhw6W8ux1lKpQt19h8",
+  authDomain: "fir-presence.firebaseapp.com",
+  databaseURL: "https://fir-presence.firebaseio.com",
+  storageBucket: "fir-presence.appspot.com",
+  messagingSenderId: "1029172247104"
+};
+
+firebase.initializeApp(config);
 
 
 // Create a variable to reference the database.
