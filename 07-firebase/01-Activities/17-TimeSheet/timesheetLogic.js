c9d46f212fb5104b50bd1a7d22a091b72ac25ca9
unknown <mrhdigital@yahoo.com>
2020-07-05 18:06:10 -0500

    added index html

---
+++ 07-firebase/01-Activities/17-TimeSheet/timesheetLogic.js
@@ -0,0 +1,18 @@
+/* global firebase moment */
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume Employee start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any attempt we use meets this test case
